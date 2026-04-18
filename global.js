f4e9788cd8a78ffac33966290c59f06d616b12c8
aaj013-creator <aaj013@ucsd.edu>
2026-04-17 21:53:24 -0700

    step 4

--- global.js
+++ global.js
@@ -37,3 +37,48 @@ for (let p of pages) {
 
     nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <label class="color-scheme">
+        Theme:
+        <select>
+            <option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>`
+);
+
+let select = document.querySelector(".color-scheme select");
+
+function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty("color-scheme", colorScheme);
+    select.value = colorScheme;
+}
+
+if ("colorScheme" in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener("input", function (event) {
+    setColorScheme(event.target.value);
+    localStorage.colorScheme = event.target.value;
+});
+
+let form = document.querySelector("form");
+
+form?.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let data = new FormData(form);
+    let url = form.action + "?";
+    let params = [];
+
+    for (let [name, value] of data) {
+        params.push(`${name}=${encodeURIComponent(value)}`);
+    }
+
+    location.href = url + params.join("&");
+});
